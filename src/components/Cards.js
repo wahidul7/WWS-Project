aebdd6901c9517969bf00f890f77c1b7afb91a2e
Md Wahidul Alam <md.alam8016@nctorontostudents.ca>
2024-10-09 19:25:10 -0400

    update-2

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,41 +5,128 @@ import CardItem from "./CardItem";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Hello World</h1>
+      <h1>Our Projects </h1>
+      <h3>
+        WWS is an experienced company, across the years we have been honored to
+        serve great companies which have permitted us to display their success
+        made possible by the services we provided. Check out this awesome case
+        studies.
+      </h3>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label="Adventure"
+              src="WWS\vonage.jpg"
+              text="Solution: Technical Support Management."
+              label="Vonage"
               path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
-              text="Travel through the Islands of Bali in a Private Cruise"
-              label="Luxury"
+              src="WWS\Movistar.jpg"
+              text="Solutions: Network Technology Services, Vendor Management."
+              label="Movistar"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\Scotiabank.jpg"
+              text="Solutions Provided: Vendor Management, Contract Management"
+              label="Scotiabank"
               path="/services"
             />
           </ul>
+          <h2>Abudabi</h2>
           <ul className="cards__items">
             <CardItem
-              src="images/img-3.jpg"
-              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-              label="Mystery"
+              src="WWS\2-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Andaz Capital Gate"
               path="/services"
             />
             <CardItem
-              src="images/img-4.jpg"
-              text="Experience Football on Top of the Himilayan Mountains"
-              label="Adventure"
-              path="/products"
+              src="WWS\1-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Etihad Towers"
+              path="/services"
             />
             <CardItem
-              src="images/img-8.jpg"
-              text="Ride through the Sahara Desert on a guided camel tour"
-              label="Adrenaline"
-              path="/sign-up"
+              src="WWS\3-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Grand Hyatt"
+              path="/services"
+            />
+          </ul>
+          <h2>Brasil</h2>
+          <ul className="cards__items">
+            <CardItem
+              src="WWS\13-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Altino Arantes Buildingn"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\14-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Birman Building"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\15-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Sede do BankBosto"
+              path="/services"
+            />
+          </ul>
+          <h2>Qatar</h2>
+          <ul className="cards__items">
+            <CardItem
+              src="WWS\6-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Four Seasons Hotel Doha"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\5-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Forty-Four West Bay Tower"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\4-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Kempinski Residences"
+              path="/services"
+            />
+          </ul>
+          <h2>Kuwait</h2>
+          <ul className="cards__items">
+            <CardItem
+              src="WWS\Diseno-sin-titulo-5-q7tuyw8s3cjfkt4ar1epxeucx3gqqn7mojzur2ch74.jpg"
+              text="Arabiya Tower"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\7-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Arraya Tower"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\8-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Panasonic Tower"
+              path="/services"
+            />
+          </ul>
+          <h2>Kuwait</h2>
+          <ul className="cards__items">
+            <CardItem
+              src="WWS\12-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Casa de la Moneda"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\10-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Eurobuilding Hotel & Suites Caracas, Maiquetia"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\11-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="Empresas Polar"
+              path="/services"
+            />
+            <CardItem
+              src="WWS\9-1-q7mwb9mak7vf0wfyeixt6hrlwjs4fk161qoqeprqj4.png"
+              text="JW Marriott Hotel Caracas"
+              path="/services"
             />
           </ul>
         </div>
--- src/components/pages/OurCompany.js
+++ src/components/pages/OurCompany.js
@@ -7,9 +7,10 @@ export default function OurCompany() {
   return (
     <>
       <h1 className="ourcompany">Our Company</h1>
-      <h1>Client Feedback</h1>
+      <h3>Client Feedback</h3>
       <h3>Our Clients Reviews</h3>
       <h3> Trusted globally by companies of all sizes</h3>
+
       <ClientFeedback />
       <Footer />
     </>
